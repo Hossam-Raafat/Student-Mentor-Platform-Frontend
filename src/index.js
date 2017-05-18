a1e8417ce8fefe58dae592a77c94bd91e92b8986
Hoda Hamad <hodahamad@aucegypt.edu>
2017-05-18 19:58:16 +0200

    setup auth for front end

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-angular
-  .module('app', []);
+
+var app = angular.module('askMak', ['ng-token-auth'])
+	.config(function($authProvider) {
+		$authProvider.configure({
+			apiUrl: 'http://localhost:3000'
+		});
+	});
+
+  app.controller('askMakCtrl', function($scope, $auth) {
+    $scope.login = function() {
+      $auth.submitLogin($scope.loginForm)
+        .then(function(resp) {
+          console.log(resp)
+        })
+        .catch(function(resp) {
+					console.log(resp)
+        });
+    };
+  });
