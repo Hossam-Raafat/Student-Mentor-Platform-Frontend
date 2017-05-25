9a3016c0c00aaa300c6a1a9cf6a30bc57ac87a00
Hossam-Raafat <juggiiboy@gmail.com>
2017-05-25 21:01:22 +0200

    Add initial student home page

--- src/app/student/auth/student_auth_controller.js
+++ src/app/student/auth/student_auth_controller.js
@@ -1,10 +1,11 @@
-angular.module('askMak').controller('studentAuthController', function ($scope, $auth) {
+angular.module('askMak').controller('studentAuthController', function ($scope, $auth, $state) {
   $scope.login = function () {
     $auth.submitLogin($scope.loginForm, {
       config: 'student' // dont forget to add {config: 'student'}
     })
       .then(function (resp) {
         console.log(resp);
+        $state.go('studentDash')
       })
       .catch(function (resp) {
         console.log(resp);
