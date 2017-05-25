104bdcfb2b046f5c8e07c2d010241472235e7b56
Taha Saleh <taha.a.saleh@gmail.com>
2017-05-25 20:56:29 +0200

    Initialize manager dashboard

---
+++ src/app/manager/dashboard/manager_dash_controller.js
@@ -0,0 +1,28 @@
+angular.module('askMak').controller('managerDashController', function ($scope, $http) {
+  $scope.resolvedQuestions = [];
+
+  $http.get('http://localhost:3000/manager/questions.json',{ params: { filter: 'resolved'} }).then(
+      function(success) {
+        console.log(success)
+        $scope.resolvedQuestions = success.data;
+        console.log($scope.resolvedQuestions)
+      },
+      function(err) {
+        console.log(err)
+      }
+  );
+
+  $scope.allQuestions = [];
+
+  $http.get('http://localhost:3000/manager/questions.json').then(
+      function(success) {
+        console.log(success)
+        $scope.allQuestions = success.data;
+        console.log($scope.allQuestions)
+      },
+      function(err) {
+        console.log(err)
+      }
+    );
+
+});
