3ecd717972ecd22c689a3423aaf4831c8ed7806d
Hoda Hamad <hodahamad@aucegypt.edu>
2017-05-29 00:58:21 +0200

    Edit mentor dashboard view

--- src/app/mentor/dashboard/mentor_dash.js
+++ src/app/mentor/dashboard/mentor_dash.js
@@ -1,3 +1,29 @@
 angular.module('askMak').controller('mentorDashController', function ($scope, $http) {
+  $scope.resolvedQuestions = [];
 
+  $http.get('http://localhost:3000/mentor/questions.json'
+  .then(
+      function(success) {
+        console.log(success)
+        $scope.resolvedQuestions = success.data;
+        console.log($scope.resolvedQuestions)
+      },
+      function(err) {
+        console.log(err)
+      }
+  )
+
+  // $scope.allQuestions = [];
+
+  // $http.get('http://localhost:3000/mentor/questions.json').then(
+  //     function(success) {
+  //       console.log(success)
+  //       $scope.allQuestions = success.data;
+  //       console.log($scope.allQuestions)
+  //     },
+  //     function(err) {
+  //       console.log(err)
+  //     }
+  //   );
+);
 });
