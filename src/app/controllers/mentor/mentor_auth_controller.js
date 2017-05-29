051ac758b7dc4a9b3b152876b7f88c6a4c18b3ff
Hoda Hamad <hodahamad@aucegypt.edu>
2017-05-29 14:07:09 +0200

    adding all files

---
+++ src/app/controllers/mentor/mentor_auth_controller.js
@@ -0,0 +1,14 @@
+angular.module('alMakinah').controller('mentorAuthController', function ($scope, $auth, AuthService) {
+  $scope.login = function () {
+    $auth.submitLogin($scope.loginForm, {
+      config: 'mentor' // dont forget to add {config: 'student'}
+    })
+      .then(function (resp) {
+        console.log(resp);
+        AuthService.loginMentor(resp);
+      })
+      .catch(function (resp) {
+        console.log(resp);
+      });
+  };
+});
