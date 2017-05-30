9e8ef8d158ea2507001ea3ae77f79dd2c7b0c02f
Hoda Hamad <hodahamad@aucegypt.edu>
2017-05-30 17:58:52 +0200

    add feature questions view in mentor dashboard

--- src/app/controllers/mentor/mentor_dash.js
+++ src/app/controllers/mentor/mentor_dash.js
@@ -1,5 +1,7 @@
 angular.module('alMakinah').controller('mentorDashController', function ($scope, $http, AuthService, $stateParams) {
   $scope.unclaimedQuestions = [];
+  $scope.claimedQuestions = [];
+  // create button that will make update request (change status of question ) and delete question from unclaimedQuestions array in frontend
 
   AuthService.logged_in_user().then(function(user){
     $scope.mentor = user;
@@ -8,20 +10,33 @@ angular.module('alMakinah').controller('mentorDashController', function ($scope,
   $http.get('http://localhost:3000/mentor/questions.json', { params: { filter: 'unclaimed'} } )
   .then(
       function(success) {
-        console.log(success)
+        console.log(success);
         $scope.unclaimedQuestions = success.data;
         console.log($scope.unclaimedQuestions)
       },
       function(err) {
-        console.log(err)
+        console.log(err);
       }
   )
-  $scope.editStatus = function(status, id) {
-    $http.put('http://localhost:3000/mentor/mentors/' + id + '.json', {mentor:{status: status}})
+  $scope.claimQuestion = function (question) {
+    $http.post('http://localhost:3000/mentor/responses.json', {response: { question_id: question.id}})
     .then(function(success) {
-      console.log(success)
+      console.log(success);
+      var questionIndex = $scope.unclaimedQuestions.indexOf(question);
+      $scope.unclaimedQuestions.splice(questionIndex, 1);
+      $scope.claimedQuestions.push(question);
+      console.log($scope.claimedQuestions)
+      },
+      function(err) {
+        console.log(err);
+      })
+    }
+  $scope.editStatus = function(status, id) {
+    $http.put('http://localhost:3000/mentor/mentors/' + id + '.json', {mentor: {status: status}})
+    .then(function(success) {
+      console.log(success);
     }, function (error) {
-      console.log(error)
+      console.log(error);
     });
   }
 });
